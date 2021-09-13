3c9cfecf6069e0e9cd9eab247b6afc2dfb679787
Jadiê Oliveira <jadieoliveira00@gmail.com>
2021-09-13 20:43:47 -0300

    Init

---
+++ MovieArievilo/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  MovieArievilo
+//
+//  Created by Jadiê on 13/09/21.
+//
+
+import UIKit
+class HomeViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("")
+        
+    }
+    
+}
+
